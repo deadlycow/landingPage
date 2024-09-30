85cebd7a126f5debca3db04dbb2dc95337e8a8c8
deadlycow-andreas <pew.andreas.k@gmail.com>
2024-09-30 17:00:58 +0200

    started with second page

--- js/main.js
+++ js/main.js
@@ -10,9 +10,6 @@ const closeRegister = document.querySelector('#closeRegister');
 const btnSignIn = document.getElementById('js-btn-signin');
 const btnReg = document.getElementById('js-btn-register');
 
-if (sigInElement.style.transform === 'translateY(0svh)' || registerElement.style.transform === 'translateY(0svh)') {
-  hero.style.width = '70svw';
-}
 
 const resizeObserver = new ResizeObserver(entries => {
   for (let entry of entries) {
@@ -20,15 +17,36 @@ const resizeObserver = new ResizeObserver(entries => {
     if (width >= 767) {
       hero.style.height = '100svh';
 
-      if (sigInElement.style.transform === 'translateY(0svh)' || sigInElement.style.transform === 'translateX(0svw)' || registerElement.style.transform === 'translateY(0svh)' || registerElement.style.transform === 'translateX(0svw)') {
+      if (sigInElement.style.transform === 'translate(0svw, 0svh)' || registerElement.style.transform === 'translate(0svw, 0svh)') {
         hero.style.width = '70svw';
+        if (sigInElement.style.transform === 'translate(0svw, 0svh)') {
+          registerElement.style.transform = 'translate(30svw, 0svh)';
+        }
+        else if (registerElement.style.transform === 'translate(0svw, 0svh)') {
+          sigInElement.style.transform = 'translate(30svw, 0svh)';
+        }
       }
+      else {
+        sigInElement.style.transform = 'translate(30svw, 0svh)';
+        registerElement.style.transform = 'translate(30svw, 0svh)';
+      }
+
     }
     else if (width < 767) {
       hero.style.width = '100svw';
 
-      if (sigInElement.style.transform === 'translateY(0svh)' || sigInElement.style.transform === 'translateX(0svw)' || registerElement.style.transform === 'translateY(0svh)' || registerElement.style.transform === 'translateX(0svw)') {
+      if (sigInElement.style.transform === 'translate(0svw, 0svh)' || registerElement.style.transform === 'translate(0svw, 0svh)') {
         hero.style.height = '50svh';
+        if (sigInElement.style.transform === 'translate(0svw, 0svh)') {
+          registerElement.style.transform = 'translate(0svw, 50svh)';
+        }
+        else if (registerElement.style.transform === 'translate(0svw, 0svh') {
+          sigInElement.style.transform = 'translate(0svw, 50svh)';
+        }
+      }
+      else {
+        sigInElement.style.transform = 'translate(0svw, 50svh)';
+        registerElement.style.transform = 'translate(0svw, 50svh)';
       }
     }
   }
@@ -38,52 +56,59 @@ resizeObserver.observe(container);
 
 closeSignIn.addEventListener('click', () => {
   if (container.getBoundingClientRect().width < 767) {
-    sigInElement.style.transform = 'translateY(50svh)';
+    sigInElement.style.transform = 'translate(0svw, 50svh)';
     hero.style.height = '100svh';
   }
   else {
-    sigInElement.style.transform = 'translateX(30svw)';
+    sigInElement.style.transform = 'translate(30svw, 0svh)';
     hero.style.width = '100svw';
   }
 })
 
 closeRegister.addEventListener('click', () => {
-  registerElement.style.transform = 'translateY(50svh)';
-  hero.style.height = '100svh';
+  if (container.getBoundingClientRect().width < 767) {
+    hero.style.height = '100svh';
+    registerElement.style.transform = 'translate(0svw, 50svh)';
+
+  }
+  else {
+    registerElement.style.transform = 'translate(30svw, 0svh)';
+    hero.style.width = '100svw';
+  }
 })
 
 btnSignIn.addEventListener('click', () => {
 
-  if (container.getBoundingClientRect().width > 767) {
+  if (container.getBoundingClientRect().width >= 767) {
     hero.style.width = '70svw';
-    if (registerElement.style.transform === 'translateX(0svw)') {
-      registerElement.style.transform = 'translateX(30svw)';
+    if (registerElement.style.transform === 'translate(0svw, 0svh)') {
+      registerElement.style.transform = 'translate(30svw, 0svh)';
     }
-    sigInElement.style.transform = 'translateX(0svw)';
+    sigInElement.style.transform = 'translate(0svw, 0svh)';
   }
   else {
-    if (registerElement.style.transform === 'translateY(0svh)') {
-      registerElement.style.transform = 'translateY(50svh)';
+    if (registerElement.style.transform === 'translate(0svw, 0svh)') {
+      registerElement.style.transform = 'translate(0svw, 50svh)';
     }
     hero.style.height = '50svh';
-    sigInElement.style.transform = 'translateY(0svh)';
+    sigInElement.style.transform = 'translate(0svw, 0svh)';
   }
 });
 
 btnReg.addEventListener('click', () => {
 
-  if (container.getBoundingClientRect().width > 767) {
+  if (container.getBoundingClientRect().width >= 767) {
     hero.style.width = '70svw';
-    if (sigInElement.style.transform === 'translateX(0svw)') {
-      sigInElement.style.transform = 'translateX(30svw)';
+    if (sigInElement.style.transform === 'translate(0svw, 0svh)') {
+      sigInElement.style.transform = 'translate(30svw, 0svh)';
     }
-    registerElement.style.transform = 'translateX(0svw)';
+    registerElement.style.transform = 'translate(0svw, 0svh)';
   }
   else {
-    if (sigInElement.style.transform === 'translateY(0svh)') {
-      sigInElement.style.transform = 'translateY(50svh)';
+    if (sigInElement.style.transform === 'translate(0svw, 0svh)') {
+      sigInElement.style.transform = 'translate(0svw, 50svh)';
     }
     hero.style.height = '50svh';
-    registerElement.style.transform = 'translateY(0svh)';
+    registerElement.style.transform = 'translate(0svw, 0svh)';
   }
 });
