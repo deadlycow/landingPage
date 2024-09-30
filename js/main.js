642dce28197823af5eb2f2009566797471079693
deadlycow-andreas <pew.andreas.k@gmail.com>
2024-09-30 10:26:39 +0200

    added close button to singin and register form, some small tweaks let

--- js/main.js
+++ js/main.js
@@ -4,24 +4,30 @@ const container = document.querySelector('.container-box');
 
 const sigInElement = document.querySelector('#signIn');
 const registerElement = document.querySelector('#register');
+const closeSignIn = document.querySelector('#closeSignIn');
+const closeRegister = document.querySelector('#closeRegister');
 
 const btnSignIn = document.getElementById('js-btn-signin');
 const btnReg = document.getElementById('js-btn-register');
 
+if (sigInElement.style.transform === 'translateY(0svh)' || registerElement.style.transform === 'translateY(0svh)') {
+  hero.style.width = '70svw';
+}
+
 const resizeObserver = new ResizeObserver(entries => {
   for (let entry of entries) {
     const { width } = entry.contentRect;
-    if (width >= 768) {
+    if (width >= 767) {
       hero.style.height = '100svh';
 
-      if (sigInElement.style.transform === 'translateY(0svh)' || registerElement.style.transform === 'translateY(0svh)') {
+      if (sigInElement.style.transform === 'translateY(0svh)' || sigInElement.style.transform === 'translateX(0svw)' || registerElement.style.transform === 'translateY(0svh)' || registerElement.style.transform === 'translateX(0svw)') {
         hero.style.width = '70svw';
       }
     }
-    else if (width < 768) {
+    else if (width < 767) {
       hero.style.width = '100svw';
 
-      if (sigInElement.style.transform === 'translateX(0svw)' || registerElement.style.transform === 'translateX(0svw)') {
+      if (sigInElement.style.transform === 'translateY(0svh)' || sigInElement.style.transform === 'translateX(0svw)' || registerElement.style.transform === 'translateY(0svh)' || registerElement.style.transform === 'translateX(0svw)') {
         hero.style.height = '50svh';
       }
     }
@@ -30,9 +36,25 @@ const resizeObserver = new ResizeObserver(entries => {
 
 resizeObserver.observe(container);
 
+closeSignIn.addEventListener('click', () => {
+  if (container.getBoundingClientRect().width < 767) {
+    sigInElement.style.transform = 'translateY(50svh)';
+    hero.style.height = '100svh';
+  }
+  else {
+    sigInElement.style.transform = 'translateX(30svw)';
+    hero.style.width = '100svw';
+  }
+})
+
+closeRegister.addEventListener('click', () => {
+  registerElement.style.transform = 'translateY(50svh)';
+  hero.style.height = '100svh';
+})
+
 btnSignIn.addEventListener('click', () => {
 
-  if (container.getBoundingClientRect().width > 768) {
+  if (container.getBoundingClientRect().width > 767) {
     hero.style.width = '70svw';
     if (registerElement.style.transform === 'translateX(0svw)') {
       registerElement.style.transform = 'translateX(30svw)';
@@ -50,7 +72,7 @@ btnSignIn.addEventListener('click', () => {
 
 btnReg.addEventListener('click', () => {
 
-  if (container.getBoundingClientRect().width > 768) {
+  if (container.getBoundingClientRect().width > 767) {
     hero.style.width = '70svw';
     if (sigInElement.style.transform === 'translateX(0svw)') {
       sigInElement.style.transform = 'translateX(30svw)';
